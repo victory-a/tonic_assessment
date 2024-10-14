f71cbdd8cdef87857963c4a45067fc9ca7e3f809
victory-a <victoryasokomeh@gmail.com>
2024-10-14 13:02:52 +0100

    feat: ui done

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Open_Sans, Manrope } from 'next/font/google';
+
+const manrope = Manrope({
+  subsets: ['latin'],
+  variable: '--body-font-family',
+  weight: ['300', '400', '500', '600', '700'],
+});
+
+const openSans = Open_Sans({
+  subsets: ['latin'],
+  variable: '--header-font-family',
+  weight: '400',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${manrope.variable} ${openSans.variable}`}>
+      <Component {...pageProps} />;
+    </main>
+  );
 }
