9331c36cdb5add0bc241f48d16893e377eb4c176
victory-a <victoryasokomeh@gmail.com>
2024-10-15 01:29:25 +0100

    feat: mobile nav

---
+++ src/components/MenuIcon.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const MenuIcon = () => {
+  return (
+    <svg width='40px' height='40px' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <g id='SVGRepo_bgCarrier' stroke-width='0'></g>
+      <g id='SVGRepo_tracerCarrier' stroke-linecap='round' stroke-linejoin='round'></g>
+      <g id='SVGRepo_iconCarrier'>
+        <rect width='24' height='24' fill='white'></rect> <path d='M6 12H18' stroke='#000000' stroke-linecap='round' stroke-linejoin='round'></path>{' '}
+        <path d='M6 15.5H18' stroke='#000000' stroke-linecap='round' stroke-linejoin='round'></path>{' '}
+        <path d='M6 8.5H18' stroke='#000000' stroke-linecap='round' stroke-linejoin='round'></path>{' '}
+      </g>
+    </svg>
+  );
+};
+
+export default MenuIcon;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,29 +1,70 @@
 import Link from 'next/link';
 import React from 'react';
+import useWindowSize from '@/hooks/useWindowSize';
+import CloseIcon from './CloseIcon';
+import MenuIcon from './MenuIcon';
 
 const Navbar = () => {
+  const { width } = useWindowSize();
+
+  const [showMenu, setShowMenu] = React.useState(false);
+
+  console.log({ showMenu });
+
+  if (width && width > 500) {
+    return (
+      <nav className='desktopNav'>
+        <div className='logo'>
+          <img src='./images/logo.svg' alt='logo' />
+          <p>Boldo</p>
+        </div>
+        <ul>
+          <li>
+            <Link href='/'>Products</Link>
+          </li>
+          <li>
+            <Link href='/'>Services</Link>
+          </li>
+          <li>
+            <Link href='/'>About</Link>
+          </li>
+          <li>
+            <Link className='loginBtn button-primary ' href='/'>
+              Log In
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+
   return (
-    <nav className='nav'>
-      <div className='logo'>
-        <img src='./images/logo.svg' alt='logo' />
-        <p>Boldo</p>
+    <nav className='mobileNav'>
+      <div className='navMenuContainer'>
+        <div className='logo'>
+          <img src='./images/logo.svg' alt='logo' />
+        </div>
+        <button onClick={() => setShowMenu(!showMenu)}>{showMenu ? <CloseIcon /> : <MenuIcon />}</button>
       </div>
-      <ul>
-        <li>
-          <Link href='/'>Products</Link>
-        </li>
-        <li>
-          <Link href='/'>Services</Link>
-        </li>
-        <li>
-          <Link href='/'>About</Link>
-        </li>
-        <li>
-          <Link className='loginBtn button-primary ' href='/'>
-            Log In
-          </Link>
-        </li>
-      </ul>
+
+      {width && width > 500 ? null : (
+        <ul style={{ height: showMenu ? 180 : 0 }}>
+          <li>
+            <Link href='/'>Products</Link>
+          </li>
+          <li>
+            <Link href='/'>Services</Link>
+          </li>
+          <li>
+            <Link href='/'>About</Link>
+          </li>
+          <li>
+            <Link className='loginBtn button-primary ' href='/'>
+              Log In
+            </Link>
+          </li>
+        </ul>
+      )}
     </nav>
   );
 };
